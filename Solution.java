3d64cf60afa5307b3ccdaf60b72b842941dfa0ff
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-11 13:05:21 -0700

    fastest possible without using hashmap, very low memory usage

--- Solution.java
+++ Solution.java
@@ -6,12 +6,8 @@ public class Solution {
     
     public int[] twoSum(int[] nums, int target) {
         for (int x = 0; x < nums.length; x++) {
-            for (int y = 0; y < nums.length; y++) {
-                if (y == x)
-                    continue;
-
+            for (int y = x+1; y < nums.length; y++) {
                 if (nums[x] + nums[y] == target) {
-                    System.gc();
                     return new int[] { x, y };
                 }
             }
